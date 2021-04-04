444a10ec9945927e657c054ff66d175a7631eadb
Lewis Carter <lewis.j.carter@gmail.com>
2021-04-04 22:03:07 +0100

    Default 404 page & the start of test scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
 	BrowserRouter as Router,
+	Route,
 	Switch
   } from "react-router-dom";
 import PrivateRoute from './Components/Routing/private.route';
@@ -8,6 +9,7 @@ import Dashboard from './Components/Dashboard/dashboard.component';
 import { BanksListing } from './Components/Money/banks.listing.component';
 import { ViewAccount } from './Components/Money/account.view.component';
 import PropertyListing from './Components/Property/property.listing.component';
+import { Error404 } from './Components/Shared/Errors/404.component';
 
 class App extends React.Component {
 	render() {
@@ -19,6 +21,8 @@ class App extends React.Component {
 				<PrivateRoute exact path="/property" component={PropertyListing} />
 				
 				<PrivateRoute exact path="/" component={Dashboard} />
+
+				<Route component={Error404} />
 			</Switch>
 		</Router>;
 	}
